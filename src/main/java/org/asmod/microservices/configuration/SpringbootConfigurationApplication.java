aff21386dc8af9db3b5b793e5c79d71f145a107e
mykasmod <mykasmod@gmail.com>
2018-06-17 12:40:24 +0800

    initial project

---
+++ src/main/java/org/asmod/microservices/configuration/SpringbootConfigurationApplication.java
@@ -0,0 +1,12 @@
+package org.asmod.microservices.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigurationApplication.class, args);
+	}
+}
